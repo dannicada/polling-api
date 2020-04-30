6ffdc0d0c8c0d949257665d6fe43f4f77c1e7358
dannicada <dannynwangwu@gmail.com>
2020-04-30 02:28:13 +0100

    initial commit, models set up

---
+++ polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .models import Poll
+
+# Create your views here.
+def polls_list(request):
+    
+
+def polls_detail(request, pk):
+    pass
