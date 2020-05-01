c9b96decc2af9bf7b54956e4e905212e475652f6
dannicada <dannynwangwu@gmail.com>
2020-05-01 15:02:29 +0100

    login functionality and authorization by token

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -2,9 +2,31 @@ from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
 from .models import Poll, Choice
-from  .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from  .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
+
+
+
+class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "wrong credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .views import polls_list, polls_detail
-from .apiviews import ChoiceList, CreateVote, PollViewSet
+from .apiviews import ChoiceList, CreateVote, PollViewSet, UserCreate, LoginView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
+
+
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename='polls')
 
 urlpatterns = [
-    # path("polls/", PollList.as_view(), name="polls_list"),
-    # path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("login/", views.obtain_auth_token, name= "login"),
+    path("users/", UserCreate.as_view(), name = "user_create"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
 ]
